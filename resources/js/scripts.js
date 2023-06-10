304cbddfc72df687b50693e646a2a50817a86d12
Rokorios <charles.cox@rokorium.com>
2023-06-10 14:37:10 -0500

    0.3
    Added GUI
    Added JS Scripts for linking to GUI
    Added CSS for Styling the HTML

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -10,17 +10,57 @@ function startGame(){
     playerChoice = playerChoice.toLowerCase();
     console.log(checkWinner(playerChoice,getComputerChoice()));
 }
+
+
+//PlayGame
+function playGame(playerChoice, computerChoice) {
+    let gameResult = checkWinner(playerChoice,computerChoice);
+    console.log(gameResult);
+    if (gameResult === 'win'){
+        document.getElementById("playerChoice").style.backgroundColor="green";
+        document.getElementById("computerChoice").style.backgroundColor="red";
+    } else if (gameResult === 'lose') {
+        document.getElementById("playerChoice").style.backgroundColor="red";
+        document.getElementById("computerChoice").style.backgroundColor="green";
+    } else {
+        document.getElementById("playerChoice").style.backgroundColor="yellow";
+        document.getElementById("computerChoice").style.backgroundColor="yellow";
+    }
+}
+//Select Scissors
+function selectScissors() {
+    playerChoice = 'scissors';
+    document.getElementById("playerChoice").setAttribute("src","resources/images/scissors.jpg");
+    playGame(playerChoice, getComputerChoice());
+}
+//Select Paper
+function selectPaper() {
+    playerChoice = 'paper';
+    document.getElementById("playerChoice").setAttribute("src","resources/images/paper.jpg");
+    playGame(playerChoice, getComputerChoice());
+}
+//Select Rock
+function selectRock() {
+    playerChoice = 'rock';
+    document.getElementById("playerChoice").setAttribute("src","resources/images/rock.jpg");
+    playGame(playerChoice, getComputerChoice());
+}
+
 //Get Computer Choice
 function getComputerChoice(){
     let computerChoice = Math.floor(Math.random()*3);
     switch (computerChoice) {
         case 0:
+            
+            document.getElementById("computerChoice").setAttribute("src","resources/images/rock.jpg");
             return('rock');
             break;
         case 1:
+            document.getElementById("computerChoice").setAttribute("src","resources/images/paper.jpg");
             return('paper');
             break;
         case 2:
+            document.getElementById("computerChoice").setAttribute("src","resources/images/scissors.jpg");
             return('scissors');
             break;
         default:
